018b1218b7f6ba236f949387c64c1711dba654a6
Daniel Tate <daniel@danieltate.io>
2025-07-01 19:15:09 +0930

    Update configuration files; modify .gitignore, adjust CI settings, and refine docker-compose for improved deployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
       <div>
         <h1>Welcome</h1>
-        <p>This is Daniel Tate's personal website.2</p>
+        <p>This is Daniel Tate's personal website.</p>
       </div>
   );
 }
